980fbce96f938e57485787c628da950bc8592548
Tq-bit <t.quante@outlook.com>
2023-09-28 15:14:37 +0200

    feat: add storage quota

--- index.js
+++ index.js
@@ -4,16 +4,42 @@ const storeName = 'localFiles';
 const storeKey = 'fileName';
 let db = null;
 
+// Methods for Storage quota
+const getStorageQuotaText = async () => {
+  const oneGigabyte = 1024 * 1024 * 1024;
+  const oneMegabyte = 1024 * 1024;
+  const oneKilobyte = 1024;
+	const estimate = await navigator.storage.estimate();
+  console.log(estimate)
+	const totalQuota = +(estimate.quota || 0);
+	const usedQuota = +(estimate.usage || 0);
+  const freeQuota = totalQuota - usedQuota;
+	return {
+    totalQuota: totalQuota > oneGigabyte ? `${(totalQuota / oneGigabyte).toFixed(2)} GB` : totalQuota > oneMegabyte ? `${(totalQuota / oneMegabyte).toFixed(2)} MB` : `${(totalQuota / oneKilobyte).toFixed(2)}KB`,
+    usedQuota: usedQuota > oneGigabyte ? `${(usedQuota / oneGigabyte).toFixed(2)} GB` : usedQuota > oneMegabyte ? `${(usedQuota / oneMegabyte).toFixed(2)} MB` : `${(usedQuota / oneKilobyte).toFixed(2)}KB`,
+    freeQuota: freeQuota > oneGigabyte ? `${(freeQuota / oneGigabyte).toFixed(2)} GB` : freeQuota > oneMegabyte ? `${(freeQuota / oneMegabyte).toFixed(2)} MB` : `${(freeQuota / oneKilobyte).toFixed(2)}KB`,
+	};
+};
+
+const renderStorageQuotaInfo = async () => {
+  const { totalQuota, usedQuota, freeQuota } = await getStorageQuotaText();
+  document.getElementById('storage-total').textContent = totalQuota;
+  document.getElementById('storage-used').textContent = usedQuota;
+  document.getElementById('storage-free').textContent = freeQuota;
+}
+
+// Methods for form buttons and file input
 const handleSubmit = async (ev) => {
 	ev.preventDefault();
 	const file = await getFileFromInput();
 	const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
 	store.add(file);
 
-  store.transaction.oncomplete = () => {
-    clearPreviousImages();
-    renderAvailableImagesFromDb();
-  }
+	store.transaction.oncomplete = () => {
+		clearPreviousImages();
+		renderAvailableImagesFromDb();
+    renderStorageQuotaInfo()
+	};
 };
 
 /**
@@ -25,7 +51,7 @@ const getFileFromInput = () => {
 		const file = document.getElementById('file').files[0];
 		const reader = new FileReader();
 		reader.onload = (event) => {
-      document.getElementById('file').value = '';
+			document.getElementById('file').value = '';
 			resolve({
 				[storeKey]: file.name,
 				data: event.target.result,
@@ -38,6 +64,14 @@ const getFileFromInput = () => {
 	});
 };
 
+const clearEntriesFromIndexedDb = () => {
+	db.transaction(storeName, 'readwrite').objectStore(storeName).clear();
+	clearPreviousImages();
+
+  renderStorageQuotaInfo();
+};
+
+// IndexedDB Methods
 /**
  * @desc Initializes the IndexedDB database
  * @param {string} dbName
@@ -61,12 +95,6 @@ const initIndexedDb = (dbName, stores) => {
 	});
 };
 
-const clearEntriesFromIndexedDb = () => {
-  console.log('Deleting entries')
-  db.transaction(storeName, 'readwrite').objectStore(storeName).clear();
-  clearPreviousImages();
-}
-
 const renderAvailableImagesFromDb = () => {
 	db.transaction(storeName, 'readonly').objectStore(storeName).openCursor().onsuccess = (event) => {
 		const cursor = event.target.result;
@@ -74,8 +102,8 @@ const renderAvailableImagesFromDb = () => {
 			const imageBuffer = cursor.value.data;
 			const imageBlog = new Blob([imageBuffer]);
 
-      const col = document.createElement('div');
-      col.classList.add('col-4');
+			const col = document.createElement('div');
+			col.classList.add('col-4');
 
 			const card = document.createElement('div');
 			card.classList.add('card');
@@ -90,7 +118,7 @@ const renderAvailableImagesFromDb = () => {
 
 			card.appendChild(image);
 			card.appendChild(title);
-      col?.appendChild(card);
+			col?.appendChild(card);
 
 			document.getElementById('images').appendChild(col);
 			cursor.continue();
@@ -99,13 +127,15 @@ const renderAvailableImagesFromDb = () => {
 };
 
 const clearPreviousImages = () => {
-  document.getElementById('images').innerHTML = '';
-}
+	document.getElementById('images').innerHTML = '';
+};
 
+// Init event listeners
 document.querySelector('form')?.addEventListener('submit', handleSubmit);
-document.querySelector('#clear-button')?.addEventListener('click', clearEntriesFromIndexedDb)
+document.querySelector('#clear-button')?.addEventListener('click', clearEntriesFromIndexedDb);
 
 window.addEventListener('load', async () => {
 	db = await initIndexedDb('my-db', [{ name: storeName, keyPath: storeKey }]);
 	renderAvailableImagesFromDb();
+  await renderStorageQuotaInfo()
 });
