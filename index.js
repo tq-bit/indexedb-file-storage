656c7bad900162ff87cc8d2c90865e0332d20a7f
Tq-bit <t.quante@outlook.com>
2023-09-29 12:02:10 +0200

    feat: add search using a cursor

--- index.js
+++ index.js
@@ -2,18 +2,11 @@
 
 const storeName = 'localFiles';
 const storeKey = 'fileName';
-const dbVersion = 1
+const dbVersion = 1;
 let db = null;
 
 const formatAsByteString = (bytes) => {
 	const oneGigabyte = 1024 * 1024 * 1024;
-// Methods for Storage quota
-/**
- * @desc Gets the storage quota text
- * @returns {Promise<{totalQuota: string, usedQuota: string, freeQuota: string}>}
- */
-const getStorageQuotaText = async () => {
-  const oneGigabyte = 1024 * 1024 * 1024;
   const oneMegabyte = 1024 * 1024;
   const oneKilobyte = 1024;
 
@@ -25,11 +18,11 @@ const getStorageQuotaText = async () => {
 	const estimate = await navigator.storage.estimate();
 	const totalQuota = +(estimate.quota || 0);
 	const usedQuota = +(estimate.usage || 0);
-  const freeQuota = totalQuota - usedQuota;
+	const freeQuota = totalQuota - usedQuota;
 	return {
-    totalQuota: totalQuota > oneGigabyte ? `${(totalQuota / oneGigabyte).toFixed(2)} GB` : totalQuota > oneMegabyte ? `${(totalQuota / oneMegabyte).toFixed(2)} MB` : `${(totalQuota / oneKilobyte).toFixed(2)}KB`,
-    usedQuota: usedQuota > oneGigabyte ? `${(usedQuota / oneGigabyte).toFixed(2)} GB` : usedQuota > oneMegabyte ? `${(usedQuota / oneMegabyte).toFixed(2)} MB` : `${(usedQuota / oneKilobyte).toFixed(2)}KB`,
-    freeQuota: freeQuota > oneGigabyte ? `${(freeQuota / oneGigabyte).toFixed(2)} GB` : freeQuota > oneMegabyte ? `${(freeQuota / oneMegabyte).toFixed(2)} MB` : `${(freeQuota / oneKilobyte).toFixed(2)}KB`,
+    totalQuota: formatAsByteString(totalQuota),
+    usedQuota: formatAsByteString(usedQuota),
+    freeQuota: formatAsByteString(freeQuota)
 	};
 };
 
@@ -44,6 +37,14 @@ const renderStorageQuotaInfo = async () => {
   document.getElementById('storage-free').textContent = freeQuota;
 }
 
+=======
+		totalQuota: formatAsByteString(totalQuota),
+		usedQuota: formatAsByteString(usedQuota),
+		freeQuota: formatAsByteString(freeQuota),
+	};
+};
+
+>>>>>>> cb25094 (feat: add search using a cursor)
 // Methods for form buttons and file input
 
 /**
@@ -58,12 +59,91 @@ const handleSubmit = async (ev) => {
 	store.add(file);
 
 	store.transaction.oncomplete = () => {
-		clearPreviousImages();
+		clearGalleryImages();
 		renderAvailableImagesFromDb();
-    renderStorageQuotaInfo()
+		renderStorageQuotaInfo();
 	};
 };
 
+const handleSearch = async (ev) => {
+	ev.preventDefault();
+	const searchInput = document.getElementById('search').value;
+	const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
+	const cursorRequest = store.openCursor();
+	clearGalleryImages();
+
+	cursorRequest.onsuccess = (event) => {
+		const cursor = event.target.result;
+		if (cursor) {
+			if (cursor.value[storeKey].toLowerCase().includes(searchInput.toLowerCase())) {
+				renderGalleryColumn(cursor);
+			}
+			cursor.continue();
+		}
+	};
+};
+
+
+
+// IndexedDB Methods
+/**
+ * @desc Initializes the IndexedDB database
+ * @param {string} dbName
+ * @param {{name: string, keyPath: string}[]} stores
+ * @returns {Promise<IDBDatabase>}
+ */
+const initIndexedDb = (dbName, stores) => {
+	return new Promise((resolve, reject) => {
+		const request = indexedDB.open(dbName, dbVersion);
+		request.onerror = (event) => {
+			reject(event.target.error);
+		};
+		request.onsuccess = (event) => {
+			resolve(event.target.result);
+		};
+		request.onupgradeneeded = (event) => {
+			stores.forEach((store) => {
+				const objectStore = event.target.result.createObjectStore(store.name, {
+					keyPath: store.keyPath,
+				});
+				objectStore.createIndex(store.keyPath, store.keyPath, { unique: true });
+			});
+		};
+	});
+};
+
+const clearEntriesFromIndexedDb = () => {
+	const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
+
+	store.clear();
+	clearGalleryImages();
+
+	store.transaction.oncomplete = () => {
+		renderStorageQuotaInfo();
+	};
+};
+
+const deleteImageFromIndexedDb = (storeKey) => {
+	const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
+	store.delete(storeKey);
+	store.transaction.oncomplete = async () => {
+		clearGalleryImages();
+		renderAvailableImagesFromDb();
+		await renderStorageQuotaInfo();
+	};
+};
+
+const renderAvailableImagesFromDb = () => {
+	db.transaction(storeName, 'readonly').objectStore(storeName).openCursor().onsuccess = (event) => {
+		const cursor = event.target.result;
+		if (cursor) {
+			renderGalleryColumn(cursor);
+			cursor.continue();
+		}
+	};
+};
+
+// Util functions
 /**
  * @desc Gets the file from the input field
  * @returns {Promise<object>}
@@ -87,6 +167,7 @@ const getFileFromInput = () => {
 		reader.readAsArrayBuffer(file);
 	});
 };
+<<<<<<< HEAD
 
 
 
@@ -211,19 +292,90 @@ const clearEntriesFromIndexedDb = () => {
 	db.transaction(storeName, 'readwrite').objectStore(storeName).clear();
 	clearPreviousImages();
   renderStorageQuotaInfo();
+=======
+const clearGalleryImages = () => {
+	document.getElementById('images').innerHTML = '';
+};
+
+const renderStorageQuotaInfo = async () => {
+	const { totalQuota, usedQuota, freeQuota } = await getStorageQuotaText();
+	document.getElementById('storage-total').textContent = totalQuota;
+	document.getElementById('storage-used').textContent = usedQuota;
+	document.getElementById('storage-free').textContent = freeQuota;
+};
+
+const renderGalleryColumn = (cursor) => {
+	const imageBuffer = cursor.value.data;
+	const imageBlog = new Blob([imageBuffer]);
+
+	const col = document.createElement('div');
+	col.classList.add('col-12', 'col-md-6', 'col-lg-4');
+
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	const cardBody = document.createElement('div');
+	cardBody.classList.add('card-body');
+
+	const image = document.createElement('img');
+	image.src = URL.createObjectURL(imageBlog);
+	image.classList.add('card-img-top');
+
+	const title = document.createElement('h5');
+	title.classList.add('card-title');
+	title.innerText = cursor.value['type'];
+
+	const subTitle = document.createElement('h6');
+	subTitle.classList.add('card-subtitle');
+	subTitle.innerText = formatAsByteString(+cursor.value['size']);
+
+	const text = document.createElement('p');
+	text.classList.add('card-text');
+	text.innerText = cursor.value[storeKey];
+
+	const deleteButton = document.createElement('button');
+	deleteButton.classList.add('btn', 'btn-danger');
+	deleteButton.innerText = 'Delete';
+	deleteButton.addEventListener('click', () => {
+		deleteImageFromIndexedDb(cursor.value[storeKey]);
+	});
+
+	cardBody.appendChild(title);
+	cardBody.appendChild(subTitle);
+	cardBody.appendChild(text);
+	cardBody.appendChild(deleteButton);
+	card.appendChild(image);
+	card.appendChild(cardBody);
+	col.appendChild(card);
+
+	document.getElementById('images').appendChild(col);
+};
+
+const formatAsByteString = (bytes) => {
+	const oneGigabyte = 1024 * 1024 * 1024;
+	const oneMegabyte = 1024 * 1024;
+	const oneKilobyte = 1024;
+
+	return bytes > oneGigabyte
+		? `${(bytes / oneGigabyte).toFixed(2)} GB`
+		: bytes > oneMegabyte
+		? `${(bytes / oneMegabyte).toFixed(2)} MB`
+		: `${(bytes / oneKilobyte).toFixed(2)}KB`;
+>>>>>>> cb25094 (feat: add search using a cursor)
 };
 
 // Init event listeners
 document.querySelector('#file-form')?.addEventListener('submit', handleSubmit);
+document.querySelector('#search-form')?.addEventListener('submit', handleSearch);
 document.querySelector('#clear-button')?.addEventListener('click', clearEntriesFromIndexedDb);
 
 window.addEventListener('load', async () => {
-	const persistent = await navigator.storage.persist()
-	if(persistent) {
+	const persistent = await navigator.storage.persist();
+	if (persistent) {
 		db = await initIndexedDb('my-db', [{ name: storeName, keyPath: storeKey }]);
 		renderAvailableImagesFromDb();
-		await renderStorageQuotaInfo()
+		await renderStorageQuotaInfo();
 	} else {
-		console.warn("Persistence is not supported");
+		console.warn('Persistence is not supported');
 	}
 });
